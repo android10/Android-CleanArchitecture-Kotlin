40f8e66f6e1027c037e18374b2ec7a1284480236
Fernando Cejas <fcejas@gmail.com>
2017-07-21 17:13:32 +0200

    Add functionality to MoviesAdapter.

--- app/src/main/kotlin/com/fernandocejas/sample/extension/Views.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/extension/Views.kt
@@ -1,7 +1,10 @@
 package com.fernandocejas.sample.extension
 
-/**
- *
- *
- * @author Fernando Cejas (the android10 coder)
- */
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View = LayoutInflater.from(context).inflate(layoutRes, this, false)
+fun ImageView.loadFromUrl(url: String): Unit = throw NotImplementedError("not implemented")
--- app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesAdapter.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesAdapter.kt
@@ -3,16 +3,23 @@ package com.fernandocejas.sample.features.movies
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.fernandocejas.sample.R
+import com.fernandocejas.sample.extension.inflate
+import kotlinx.android.synthetic.main.row_movie.view.*
 import javax.inject.Inject
 
 class MoviesAdapter @Inject constructor() : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
     var movies: List<MovieViewModel> = emptyList()
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent.inflate(R.layout.row_movie))
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) = viewHolder.bind(movies[position])
 
     override fun getItemCount() = movies.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movieViewModel: MovieViewModel?) {
+            itemView.title.text = movieViewModel?.title
+        }
+    }
 }
