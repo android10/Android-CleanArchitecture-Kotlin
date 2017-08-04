f1d274bc027b3344824c8a9f17b6d961ac4d7260
Fernando Cejas <fcejas@gmail.com>
2017-08-04 23:05:35 +0200

    Add JobExecutor.

--- app/src/main/kotlin/com/fernandocejas/sample/framework/executor/ThreadScheduler.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/framework/executor/ThreadScheduler.kt
@@ -4,8 +4,10 @@ import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class ThreadScheduler
+
+@Singleton class ThreadScheduler
 @Inject constructor() : ExecutionScheduler {
     override fun ui(): Scheduler = AndroidSchedulers.mainThread()
     override fun highPriority(): Scheduler = Schedulers.newThread() //TODO: use thread pool
