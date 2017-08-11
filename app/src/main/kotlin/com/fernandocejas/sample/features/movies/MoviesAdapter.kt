43fd2dffe89e2bb937cd734d968f3c2c1fb9c66e
Fernando Cejas <fcejas@gmail.com>
2017-08-11 15:01:37 +0200

    Rename adapter movies field to collection. Use delegated property on adapter to notify dataset change.

--- app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesAdapter.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesAdapter.kt
@@ -7,17 +7,20 @@ import com.fernandocejas.sample.R
 import com.fernandocejas.sample.framework.extension.inflate
 import kotlinx.android.synthetic.main.row_movie.view.*
 import javax.inject.Inject
+import kotlin.properties.Delegates
 
 class MoviesAdapter
 @Inject constructor() : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
-    internal var movies: List<MovieViewModel> = emptyList()
+    internal var collection: List<MovieViewModel> by Delegates.observable(emptyList()) {
+        property, old, new -> notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent.inflate(R.layout.row_movie))
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) = viewHolder.bind(movies[position])
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) = viewHolder.bind(collection[position])
 
-    override fun getItemCount() = movies.size
+    override fun getItemCount() = collection.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(movieViewModel: MovieViewModel?) {
