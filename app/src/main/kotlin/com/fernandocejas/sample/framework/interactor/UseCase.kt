6276658f74d24f6ae70e47851205839dd6878a8d
Fernando Cejas <fcejas@gmail.com>
2017-08-12 00:00:21 +0200

    Using with<receiver> in UseCase.

--- app/src/main/kotlin/com/fernandocejas/sample/framework/interactor/UseCase.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/framework/interactor/UseCase.kt
@@ -1,6 +1,5 @@
 package com.fernandocejas.sample.framework.interactor
 
-import android.util.Log
 import com.fernandocejas.sample.framework.executor.ExecutionScheduler
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -15,14 +14,11 @@ abstract class UseCase<T, in Params>(private val scheduler: ExecutionScheduler) 
 
     fun execute(observer: UseCaseObserver<T>, params: Params? = null) {
         val observable = buildObservable(params)
-
-        observable.subscribeOn(Schedulers.newThread())
-        observable.observeOn(AndroidSchedulers.mainThread())
-        //-------
-        observable.doOnNext { Log.d("FernandoAndroid", "Fer -----> " + it.toString()) }
-        observable.subscribe { Log.d("FernandoAndroid", "Fer -----> " + it.toString()) }
-        //-------
-        disposables.add(observable.subscribeWith(observer))
+        with(observable) {
+            subscribeOn(Schedulers.newThread())
+            observeOn(AndroidSchedulers.mainThread())
+            disposables.add(subscribeWith(observer))
+        }
     }
 
     fun dispose() = disposables.dispose()
