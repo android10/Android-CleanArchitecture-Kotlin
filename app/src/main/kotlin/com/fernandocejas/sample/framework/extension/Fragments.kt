0b4a89392c0a21634e482fb8c4455be2b75b6c74
Fernando Cejas <fcejas@gmail.com>
2017-08-31 15:57:06 +0200

    Better extension function for adding fragments.

--- app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Fragments.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Fragments.kt
@@ -3,8 +3,5 @@ package com.fernandocejas.sample.framework.extension
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 
-inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
-    val fragmentTransaction = beginTransaction()
-    fragmentTransaction.func()
-    fragmentTransaction.commit()
-}
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) =
+        beginTransaction().func().commit()
