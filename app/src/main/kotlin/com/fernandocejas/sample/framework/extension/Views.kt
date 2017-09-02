b63863804e29cd9ae90e769de881fec44c49ca59
Fernando Cejas <fcejas@gmail.com>
2017-09-02 17:30:10 +0200

    Change style for movie previews.

--- app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Views.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Views.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View = LayoutInflater.from(context).inflate(layoutRes, this, false)
-fun ImageView.loadFromUrl(url: String) = Glide.with(this.context.applicationContext).load(url).into(this)
+fun ImageView.loadFromUrl(url: String) = Glide.with(this.context.applicationContext).load(url).transition(DrawableTransitionOptions.withCrossFade()).into(this)!!
