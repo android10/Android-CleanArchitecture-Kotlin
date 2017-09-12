8fac8e9315499ebdb283d47787a74650c90ec7f2
Fernando Cejas <fcejas@gmail.com>
2017-09-12 17:45:07 +0200

    Forgot to push changes which fix build.

--- app/src/main/kotlin/com/fernandocejas/sample/features/movies/MovieDetailsEntity.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MovieDetailsEntity.kt
@@ -1,9 +1,26 @@
 package com.fernandocejas.sample.features.movies
 
-/**
- *
- *
- * @author Fernando Cejas (the android10 coder)
- */
-class MovieDetailsEntity {
+data class MovieDetailsEntity(private val id: Int,
+                              private val title: String,
+                              private val poster: String,
+                              private val summary: String,
+                              private val cast: String,
+                              private val director: String,
+                              private val year: Int,
+                              private val trailer: String) {
+
+    fun toMovieDetails(): MovieDetails {
+        return MovieDetails.create {
+            this@MovieDetailsEntity.let {
+                id = it.id
+                title = it.title
+                poster = it.poster
+                summary = it.summary
+                cast = it.cast
+                director = it.director
+                year = it.year
+                trailer = it.trailer
+            }
+        }
+    }
 }
