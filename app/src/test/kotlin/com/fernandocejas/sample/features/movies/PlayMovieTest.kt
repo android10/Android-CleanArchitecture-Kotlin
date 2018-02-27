8b0c2b54bd8b8e73972a55deb875630ed4d09278
Fernando Cejas <fcejas@gmail.com>
2018-02-27 16:24:31 +0100

    Fix PlayMovieTest

--- app/src/test/kotlin/com/fernandocejas/sample/features/movies/PlayMovieTest.kt
+++ app/src/test/kotlin/com/fernandocejas/sample/features/movies/PlayMovieTest.kt
@@ -1,17 +1,21 @@
 package com.fernandocejas.sample.features.movies
 
+import android.content.Context
 import com.fernandocejas.sample.AndroidTest
 import com.nhaarman.mockito_kotlin.any
-import org.amshove.kluent.shouldBe
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 
 class PlayMovieTest : AndroidTest() {
 
     private lateinit var playMovie: PlayMovie
 
+    @Mock private lateinit var context: Context
+
     @Before fun setUp() {
-        playMovie = PlayMovie(context())
+        playMovie = PlayMovie(context)
     }
 
     @Test fun `should play movie trailer`() {
@@ -19,6 +23,6 @@ class PlayMovieTest : AndroidTest() {
 
         playMovie.execute({}, params)
 
-        true shouldBe true //TODO: check shouldNavigateTo
+        verify(context).startActivity(any())
     }
 }
