4011bfdc7061ece0305b84b58041baa0860dafb1
Fernando Cejas <fcejas@gmail.com>
2018-04-08 19:22:54 +0200

    Add context dependency on Network data source.

--- app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesRepository.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesRepository.kt
@@ -1,5 +1,6 @@
 package com.fernandocejas.sample.features.movies
 
+import android.content.Context
 import com.fernandocejas.sample.framework.exception.Failure
 import com.fernandocejas.sample.framework.exception.Failure.NetworkConnection
 import com.fernandocejas.sample.framework.exception.Failure.ServerError
@@ -13,7 +14,8 @@ interface MoviesRepository {
     fun movieDetails(movieId: Int): Either<Failure, MovieDetails>
 
     class Network
-    @Inject constructor(private val service: MoviesService) : MoviesRepository {
+    @Inject constructor(private val context: Context,
+                        private val service: MoviesService) : MoviesRepository {
 
         override fun movies(): Either<Failure, List<Movie>> {
             return when (isConnectedToNetwork()) {
