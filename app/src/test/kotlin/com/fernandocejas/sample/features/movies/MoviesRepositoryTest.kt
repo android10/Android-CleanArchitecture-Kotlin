13c16943871f3662082ac0faedcdcd496b783d16
Fernando Cejas <fcejas@gmail.com>
2018-04-11 12:11:36 +0200

    Add more unit test cases for Network data source in MoviesRepository.

--- app/src/test/kotlin/com/fernandocejas/sample/features/movies/MoviesRepositoryTest.kt
+++ app/src/test/kotlin/com/fernandocejas/sample/features/movies/MoviesRepositoryTest.kt
@@ -2,11 +2,18 @@ package com.fernandocejas.sample.features.movies
 
 import com.fernandocejas.sample.UnitTest
 import com.fernandocejas.sample.features.movies.MoviesRepository.Network
+import com.fernandocejas.sample.framework.exception.Failure.NetworkConnection
+import com.fernandocejas.sample.framework.exception.Failure.ServerError
 import com.fernandocejas.sample.framework.extension.empty
+import com.fernandocejas.sample.framework.functional.Either
+import com.fernandocejas.sample.framework.functional.Either.Left
 import com.fernandocejas.sample.framework.functional.Either.Right
 import com.fernandocejas.sample.framework.platform.NetworkHandler
 import com.nhaarman.mockito_kotlin.given
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeInstanceOf
 import org.amshove.kluent.shouldEqual
 import org.junit.Before
 import org.junit.Test
@@ -56,7 +63,38 @@ class MoviesRepositoryTest : UnitTest() {
         verify(service).movies()
     }
 
-    @Test fun `should return empty details by default`() {
+    @Test fun `movies service should return network failure when no connection`() {
+        given { networkHandler.isConnected }.willReturn(false)
+
+        val movies = networkRepository.movies()
+
+        movies shouldBeInstanceOf Either::class.java
+        movies.isLeft shouldEqual true
+        movies.either({ failure -> failure shouldBeInstanceOf NetworkConnection::class.java }, {})
+        verifyZeroInteractions(service)
+    }
+
+    @Test fun `movies service should return server error if no successful response`() {
+        given { networkHandler.isConnected }.willReturn(true)
+
+        val movies = networkRepository.movies()
+
+        movies shouldBeInstanceOf Either::class.java
+        movies.isLeft shouldEqual true
+        movies.either({ failure -> failure shouldBeInstanceOf ServerError::class.java }, {})
+    }
+
+    @Test fun `movies request should catch exceptions`() {
+        given { networkHandler.isConnected }.willReturn(true)
+
+        val movies = networkRepository.movies()
+
+        movies shouldBeInstanceOf Either::class.java
+        movies.isLeft shouldEqual true
+        movies.either({ failure -> failure shouldBeInstanceOf ServerError::class.java }, {})
+    }
+
+    @Test fun `should return empty movie details by default`() {
         given { networkHandler.isConnected }.willReturn(true)
         given { movieDetailsResponse.body() }.willReturn(null)
         given { movieDetailsResponse.isSuccessful }.willReturn(true)
@@ -83,5 +121,36 @@ class MoviesRepositoryTest : UnitTest() {
         movieDetails shouldEqual Right(MovieDetails(8, "title", String.empty(), String.empty(),
                 String.empty(), String.empty(), 0, String.empty()))
         verify(service).movieDetails(1)
+    }
+
+    @Test fun `movie details service should return network failure when no connection`() {
+        given { networkHandler.isConnected }.willReturn(false)
+
+        val movieDetails = networkRepository.movieDetails(1)
+
+        movieDetails shouldBeInstanceOf Either::class.java
+        movieDetails.isLeft shouldEqual true
+        movieDetails.either({ failure -> failure shouldBeInstanceOf NetworkConnection::class.java }, {})
+        verifyZeroInteractions(service)
+    }
+
+    @Test fun `movie details service should return server error if no successful response`() {
+        given { networkHandler.isConnected }.willReturn(true)
+
+        val movieDetails = networkRepository.movieDetails(1)
+
+        movieDetails shouldBeInstanceOf Either::class.java
+        movieDetails.isLeft shouldEqual true
+        movieDetails.either({ failure -> failure shouldBeInstanceOf ServerError::class.java }, {})
+    }
+
+    @Test fun `movie details request should catch exceptions`() {
+        given { networkHandler.isConnected }.willReturn(true)
+
+        val movieDetails = networkRepository.movieDetails(1)
+
+        movieDetails shouldBeInstanceOf Either::class.java
+        movieDetails.isLeft shouldEqual true
+        movieDetails.either({ failure -> failure shouldBeInstanceOf ServerError::class.java }, {})
     }
 }
