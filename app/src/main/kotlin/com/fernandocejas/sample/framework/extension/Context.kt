092f6cf4afad581b9afbc5af3bba2e32c08390e2
Fernando Cejas <fcejas@gmail.com>
2018-04-11 12:31:39 +0200

    Specify explicit type in Context.networkInfo propery extension.

--- app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Context.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/framework/extension/Context.kt
@@ -2,7 +2,7 @@ package com.fernandocejas.sample.framework.extension
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkInfo
 
-val Context.networkInfo get() =
+val Context.networkInfo: NetworkInfo get() =
     (this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
-
