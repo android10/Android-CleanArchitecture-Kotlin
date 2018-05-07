347fafcea58d7ca61790f40f80ba9275ed435edf
Fernando Cejas <fcejas@gmail.com>
2018-05-07 16:45:23 +0200

    Add flatmap and map to monadic type Either<L, R>

--- app/src/main/kotlin/com/fernandocejas/sample/core/functional/Either.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/functional/Either.kt
@@ -31,8 +31,10 @@ sealed class Either<out L, out R> {
     data class Right<out R>(val b: R) : Either<Nothing, R>()
 
     val isRight get() = this is Right<R>
-
     val isLeft get() = this is Left<L>
+
+    fun <L> left(a: L) = Either.Left(a)
+    fun <R> right(b: R) = Either.Right(b)
 
     fun either(fnL: (L) -> Any, fnR: (R) -> Any): Any =
             when (this) {
@@ -40,3 +42,17 @@ sealed class Either<out L, out R> {
                 is Right -> fnR(b)
             }
 }
+
+// Credits to Alex Hart -> https://proandroiddev.com/kotlins-nothing-type-946de7d464fb
+// Composes 2 functions
+fun <A, B, C> ((A) -> B).c(f: (B) -> C): (A) -> C = {
+    f(this(it))
+}
+
+fun <T, L, R> Either<L, R>.flatMap(fn: (R) -> Either<L, T>): Either<L, T> =
+        when (this) {
+            is Either.Left -> Either.Left(a)
+            is Either.Right -> fn(b)
+        }
+
+fun <T, L, R> Either<L, R>.map(fn: (R) -> (T)): Either<L, T> = this.flatMap(fn.c(::right))
