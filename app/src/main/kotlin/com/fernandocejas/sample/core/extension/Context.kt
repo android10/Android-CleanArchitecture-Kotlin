26c4905e1a44c4589638f19ceaafd86fe1898e72
chronvas <chron.vas@outlook.com>
2018-05-13 19:42:47 +0100

    Refactored Context extension on Context.kt to return a nullable NetworkInfo Refactored NetworkHandler Refactored usage in MoviesRepository by adding missing "null" case Added test for null return from networkHandler.isConnected

--- app/src/main/kotlin/com/fernandocejas/sample/core/extension/Context.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/extension/Context.kt
@@ -19,5 +19,5 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 
-val Context.networkInfo: NetworkInfo get() =
+val Context.networkInfo: NetworkInfo? get() =
     (this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
--- app/src/main/kotlin/com/fernandocejas/sample/core/platform/NetworkHandler.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/platform/NetworkHandler.kt
@@ -21,10 +21,10 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
- * Injectable class which handles device network connection.
+ * Injectable class which returns information about the network connection state.
  */
 @Singleton
 class NetworkHandler
 @Inject constructor(private val context: Context) {
-    val isConnected get() = context.networkInfo.isConnectedOrConnecting
+    val isConnected get() = context.networkInfo?.isConnectedOrConnecting
 }
