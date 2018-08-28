753c3730d8ec9592816f5e01e2de5097005dd38e
Artyom <tioma.master@gmail.com>
2018-08-28 16:49:19 +0300

    Update AndroidAssertions.kt

--- app/src/test/kotlin/com/fernandocejas/sample/AndroidAssertions.kt
+++ app/src/test/kotlin/com/fernandocejas/sample/AndroidAssertions.kt
@@ -24,7 +24,7 @@ import org.robolectric.Robolectric
 import org.robolectric.Shadows
 import kotlin.reflect.KClass
 
-infix fun KClass<out AppCompatActivity>.`shouldNavigateTo`(nextActivity: KClass<out AppCompatActivity>) = {
+infix fun KClass<out AppCompatActivity>.`shouldNavigateTo`(nextActivity: KClass<out AppCompatActivity>) {
     val originActivity = Robolectric.buildActivity(this.java).get()
     val shadowActivity = Shadows.shadowOf(originActivity)
     val nextIntent = shadowActivity.peekNextStartedActivity()
