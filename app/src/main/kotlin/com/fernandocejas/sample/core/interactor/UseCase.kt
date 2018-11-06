4e914d263b5a55190e565599f3c2dfa4ed282b60
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2018-11-06 23:09:46 +0100

    Update the coroutines implementation to the new sintax

--- app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
@@ -17,10 +17,10 @@ package com.fernandocejas.sample.core.interactor
 
 import com.fernandocejas.sample.core.exception.Failure
 import com.fernandocejas.sample.core.functional.Either
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 /**
  * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
@@ -35,8 +35,10 @@ abstract class UseCase<out Type, in Params> where Type : Any {
     abstract suspend fun run(params: Params): Either<Failure, Type>
 
     operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
-        val job = async(CommonPool) { run(params) }
-        launch(UI) { onResult(job.await()) }
+        GlobalScope.launch(Dispatchers.Main) {
+            val job = async(Dispatchers.IO) { run(params) }
+            onResult(job.await())
+        }
     }
 
     class None
