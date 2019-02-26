d3e7d57531b8ba34ab81615c0bb2ff4cf4fc3fff
zhanggong <zhanggong.m@gmail.com>
2019-02-26 13:46:31 +0800

    replaced experimental kotline coroutine api to release.

--- app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
@@ -17,10 +17,7 @@ package com.fernandocejas.sample.core.interactor
 
 import com.fernandocejas.sample.core.exception.Failure
 import com.fernandocejas.sample.core.functional.Either
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.*
 
 /**
  * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
@@ -34,9 +31,9 @@ abstract class UseCase<out Type, in Params> where Type : Any {
 
     abstract suspend fun run(params: Params): Either<Failure, Type>
 
-    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
-        val job = async(CommonPool) { run(params) }
-        launch(UI) { onResult(job.await()) }
+    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) = runBlocking{
+        val job = async(Dispatchers.IO) { run(params) }
+        CoroutineScope(Dispatchers.Main).launch { onResult(job.await()) }
     }
 
     class None
