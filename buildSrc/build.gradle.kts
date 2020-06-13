99eec4e8ab9f5562b779e62de0925b5341432a4d
Fernando Cejas <fcejas@gmail.com>
2020-06-13 19:47:25 +0200

    Add Android build tools dependency to buildScr build.gradle.kts file.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,17 @@
-repositories {
-    jcenter()
-    google()
+object Dependencies {
+    const val AndroidBuildTools = "com.android.tools.build:gradle:4.0.0"
 }
 
 plugins {
     `kotlin-dsl`
     `kotlin-dsl-precompiled-script-plugins`
+}
+
+repositories {
+    jcenter()
+    google()
+}
+
+dependencies {
+    implementation(Dependencies.AndroidBuildTools)
 }
