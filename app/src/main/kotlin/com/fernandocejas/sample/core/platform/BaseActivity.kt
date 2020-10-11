52616e2e62c200505aca6826b2b7921d6d76c2d3
Fernando Cejas <fcejas@gmail.com>
2020-10-11 16:34:06 +0200

    Fix R import on BaseActivity class.

--- app/src/main/kotlin/com/fernandocejas/sample/core/platform/BaseActivity.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/platform/BaseActivity.kt
@@ -17,8 +17,7 @@ package com.fernandocejas.sample.core.platform
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.fernandocejas.sample.R.id
-import com.fernandocejas.sample.R.layout
+import com.fernandocejas.sample.R
 import com.fernandocejas.sample.core.extension.inTransaction
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -32,18 +31,18 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_layout)
+        setContentView(R.layout.activity_layout)
         setSupportActionBar(toolbar)
         addFragment(savedInstanceState)
     }
 
     override fun onBackPressed() {
-        (supportFragmentManager.findFragmentById(id.fragmentContainer) as BaseFragment).onBackPressed()
+        (supportFragmentManager.findFragmentById(R.id.fragmentContainer) as BaseFragment).onBackPressed()
         super.onBackPressed()
     }
 
     private fun addFragment(savedInstanceState: Bundle?) =
-            savedInstanceState ?: supportFragmentManager.inTransaction { add(id.fragmentContainer, fragment()) }
+            savedInstanceState ?: supportFragmentManager.inTransaction { add(R.id.fragmentContainer, fragment()) }
 
     abstract fun fragment(): BaseFragment
 }
