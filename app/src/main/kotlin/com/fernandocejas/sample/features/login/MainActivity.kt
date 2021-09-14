8e865e9b3ea00bba72aee413b0925b9791988781
ninhconggiang <ninhconggiang94@gmail.com>
2021-09-14 17:22:46 +0700

    no message

--- app/src/main/kotlin/com/fernandocejas/sample/features/login/MainActivity.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/login/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.fernandocejas.sample.features.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.fernandocejas.sample.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logIn.setOnClickListener {
+            val intent:Intent=Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.login_fragment, container, false)
+//    }
+
 }
