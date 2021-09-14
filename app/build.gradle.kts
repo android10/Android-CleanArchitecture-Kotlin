149e9fd06fc46ac83816e710a20f3a66e0887902
ninhconggiang <ninhconggiang94@gmail.com>
2021-09-14 16:28:57 +0700

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
   id(ScriptPlugins.variants)
   id(ScriptPlugins.quality)
   id(ScriptPlugins.compilation)
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -34,7 +35,10 @@ android {
 }
 
 dependencies {
-  //Compile time dependencies
+  implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    //Compile time dependencies
   kapt(Libraries.lifecycleCompiler)
   kapt(Libraries.hiltCompiler)
 
---
+++ app/src/main/kotlin/com/fernandocejas/sample/features/login/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fernandocejas.sample.features.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fernandocejas.sample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
