0d2b72319704be3b0cf9710d5f0e003df9de0e0a
Matthew Zavislak <elevenfive@users.noreply.github.com>
2021-09-30 18:41:41 -0700

    Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,13 @@
 object Kotlin {
-    const val standardLibrary = "1.4.10"
-    const val coroutines = "1.3.9"
+    /** Must update build.gradle.kts when changing this </a> */
+    const val standardLibrary = "1.5.31"
+
+    const val coroutines = "1.5.2"
 }
 
 object AndroidSdk {
     const val min = 26
-    const val compile = 29
+    const val compile = 30
     const val target = compile
 }
 
@@ -18,11 +20,18 @@ object AndroidClient {
 
 object BuildPlugins {
     object Versions {
-        const val buildToolsVersion = "4.2.0"
-        const val gradleVersion = "6.8"
-        const val hilt = "2.35.1"
+        /** Must update build.gradle.kts when changing this </a> */
+        const val buildToolsVersion = "7.0.2"
+
+        /**  Must update gradle/wrapper/gradle-wrapper.properties when changing this */
+        const val gradleVersion = "7.2"
+
+        const val hilt = "2.39"
+
+        const val versions = "0.39.0"
     }
 
+    const val versionsPlugin = "com.github.ben-manes.versions"
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.buildToolsVersion}"
     const val kotlinGradlePlugin =
         "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.standardLibrary}"
@@ -31,7 +40,7 @@ object BuildPlugins {
     const val kotlinAndroid = "kotlin-android"
     const val kotlinKapt = "kotlin-kapt"
     const val androidHilt = "dagger.hilt.android.plugin"
-    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+    const val androidExtensions = "android.extensions"
 }
 
 object ScriptPlugins {
@@ -44,16 +53,16 @@ object ScriptPlugins {
 object Libraries {
     private object Versions {
         const val hilt = BuildPlugins.Versions.hilt
-        const val appCompat = "1.2.0"
-        const val constraintLayout = "2.0.2"
-        const val recyclerView = "1.1.0"
+        const val appCompat = "1.3.1"
+        const val constraintLayout = "2.1.1"
+        const val recyclerView = "1.2.1"
         const val cardView = "1.0.0"
         const val material = "1.1.0"
-        const val lifecycle = "2.2.0"
-        const val lifecycleExtensions = "2.1.0"
-        const val annotations = "1.1.0"
-        const val ktx = "1.3.2"
-        const val glide = "4.11.0"
+        const val lifecycle = "2.3.1"
+        const val annotations = "1.2.0"
+        const val ktx = "1.6.0"
+        const val fragmentKtx = "1.3.6"
+        const val glide = "4.12.0"
         const val retrofit = "2.9.0"
         const val okHttpLoggingInterceptor = "4.9.0"
     }
@@ -67,11 +76,10 @@ object Libraries {
     const val constraintLayout =
         "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
     const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
+    const val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
     const val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
     const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
     const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
-    const val lifecycleExtensions =
-        "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleExtensions}"
     const val cardView = "androidx.cardview:cardview:${Versions.cardView}"
     const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
     const val material = "com.google.android.material:material:${Versions.material}"
@@ -86,15 +94,13 @@ object Libraries {
 
 object TestLibraries {
     private object Versions {
-        const val junit4 = "4.13.1"
-        const val mockk = "1.10.0"
-        const val robolectric = "4.4"
-        const val kluent = "1.14"
-        const val testRunner = "1.1.0"
-        const val espressoCore = "3.2.0"
-        const val espressoIntents = "3.1.0"
-        const val testExtensions = "1.1.1"
-        const val testRules = "1.1.0"
+        const val junit4 = "4.13.2"
+        const val mockk = "1.12.0"
+        const val robolectric = "4.6.1"
+        const val kluent = "1.68"
+        const val testRunner = "1.4.0"
+        const val espresso = "3.4.0"
+        const val testExtensions = "1.1.3"
         const val hiltTesting = BuildPlugins.Versions.hilt
     }
 
@@ -103,17 +109,16 @@ object TestLibraries {
     const val robolectric = "org.robolectric:robolectric:${Versions.robolectric}"
     const val kluent = "org.amshove.kluent:kluent:${Versions.kluent}"
     const val testRunner = "androidx.test:runner:${Versions.testRunner}"
-    const val testRules = "androidx.test:rules:${Versions.testRules}"
-    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
-    const val espressoIntents =
-        "androidx.test.espresso:espresso-intents:${Versions.espressoIntents}"
+    const val testRules = "androidx.test:rules:${Versions.testRunner}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val espressoIntents = "androidx.test.espresso:espresso-intents:${Versions.espresso}"
     const val testExtJunit = "androidx.test.ext:junit:${Versions.testExtensions}"
     const val hiltTesting = "com.google.dagger:hilt-android-testing:${Versions.hiltTesting}"
 }
 
 object DevLibraries {
     private object Versions {
-        const val leakCanary = "2.5"
+        const val leakCanary = "2.7"
     }
 
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanary}"
