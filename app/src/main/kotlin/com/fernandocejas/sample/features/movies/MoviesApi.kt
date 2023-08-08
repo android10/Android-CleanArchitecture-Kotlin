5c161125f4447e1ce48256dd57f32813639a8b66
Fernando Cejas <android10@fernandocejas.com>
2023-08-08 17:58:33 +0200

    ref: some indentation

--- app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesApi.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MoviesApi.kt
@@ -28,6 +28,7 @@ internal interface MoviesApi {
 
     @GET(MOVIES)
     fun movies(): Call<List<MovieEntity>>
+
     @GET(MOVIE_DETAILS)
     fun movieDetails(@Path(PARAM_MOVIE_ID) movieId: Int): Call<MovieDetailsEntity>
 }
