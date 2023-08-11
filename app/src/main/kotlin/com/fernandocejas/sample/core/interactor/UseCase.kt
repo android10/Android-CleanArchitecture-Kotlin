9f335278de018ea604e655f81ebef9963be611b6
Fernando Cejas <android10@fernandocejas.com>
2023-08-11 15:29:38 +0200

    ref: UseCase class improvement and extra doc/comments

--- app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
@@ -17,15 +17,26 @@ package com.fernandocejas.sample.core.interactor
 
 import com.fernandocejas.sample.core.failure.Failure
 import com.fernandocejas.sample.core.functional.Either
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 /**
- * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
- * This abstraction represents an execution unit for different use cases (this means that any use
- * case in the application should implement this contract).
+ * Abstract class for a Use Case (Interactor in terms of
+ * Clean Architecture naming convention).
  *
- * By convention each [UseCase] implementation will execute its job in a background thread
- * (kotlin coroutine) and will post the result in the UI thread.
+ * This abstraction represents an execution unit for
+ * different use cases (this means that any use case
+ * in the application should implement this contract).
+ *
+ * By convention each [UseCase] implementation will
+ * execute its job in a pool of threads using
+ * [Dispatchers.IO].
+ *
+ * The result of the computation will be posted on the
+ * same thread used by the @param 'scope' [CoroutineScope].
  */
 abstract class UseCase<out Type, in Params> where Type : Any {
 
@@ -33,16 +44,23 @@ abstract class UseCase<out Type, in Params> where Type : Any {
 
     operator fun invoke(
         params: Params,
-        scope: CoroutineScope = GlobalScope,
+        scope: CoroutineScope = MainScope(),
         onResult: (Either<Failure, Type>) -> Unit = {}
     ) {
-        scope.launch(Dispatchers.Main) {
-            val deferred = async(Dispatchers.IO) {
+        scope.launch {
+            val deferredJob = async(Dispatchers.IO) {
                 run(params)
             }
-            onResult(deferred.await())
+            onResult(deferredJob.await())
         }
     }
 
+    /**
+     * Helper class to represent Empty
+     * Params when a use case does not
+     * need them.
+     *
+     * @see UseCase
+     */
     class None
 }
