afb584d59470a1ce98cd249f1937f518dc86061c
Fernando Cejas <android10@fernandocejas.com>
2024-08-08 15:21:57 +0200

    chore: bump dependencies, compile and target Sdk

---
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/ui/NewMoviesActivity.kt
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2020 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fernandocejas.sample.features.movies.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.ComponentActivity
+
+@SuppressLint("RestrictedApi")
+class NewMoviesActivity : ComponentActivity() {
+
+    companion object {
+        fun callingIntent(context: Context) = Intent(context, NewMoviesActivity::class.java)
+    }
+
+    fun fragment() = MoviesFragment()
+}
